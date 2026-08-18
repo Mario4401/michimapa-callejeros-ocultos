1114f6b7f55f5f9adf6ac9098b85bf11a42f9420
Mario4401 <278123966+Mario4401@users.noreply.github.com>
2026-08-18 18:15:15 +0200

    Añadir sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE='michimapa-v1';
+const FILES=['./','index.html','styles.css','game.js','manifest.webmanifest','icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
